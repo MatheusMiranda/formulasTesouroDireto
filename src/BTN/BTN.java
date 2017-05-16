023765911abaf9e684e6a23911919e7b530a3c56
Matheus Miranda <matheusmirandalacerda@gmail.com>
2017-05-16 08:58:33 -0300

    Create initial test of "Preço Unitário" in BTN
    Signed-off-by: arthurTemporim <arthurrtl@gmail.com>
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- src/BTN/BTN.java
+++ src/BTN/BTN.java
@@ -1,6 +1,6 @@
 package BTN;
 
-
 public class BTN {
+	
 
 }
--- src/BTN/BTNTest.java
+++ src/BTN/BTNTest.java
@@ -1,14 +1,30 @@
 package BTN;
 
 import static org.junit.Assert.*;
+import org.junit.Before;
 
 import org.junit.Test;
 
 public class BTNTest {
 
+	BTN btn;
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
 	}
+	
+	@Before
+	public void setUp() throws Exception {
+		btn = new BTN();
+	}
+	
+	@Test
+	public void testCalculoPrecoUnitarioAtualizado() {
+		double precoUnitarioAnterior = 10.0;
+		double taxaReferencial = 0.5;
+		assertEquals(5.0, btn.calculaPrecoUnitarioAtualizado(precoUnitarioAnterior,taxaReferencial));
+	}
+	
 
 }
