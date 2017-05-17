61ac7319700e430f8eb1e985225ab46aa904b143
arthurTemporim <arthurrtl@gmail.com>
2017-05-17 17:17:17 -0300

    Create test of "descobreDeflator" in BTN class.
    Signed-off-by: arthurTemporim <arthurrtl@gmail.com>
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- src/BTN/BTNTest.java
+++ src/BTN/BTNTest.java
@@ -1,8 +1,10 @@
 package BTN;
 
-import static org.junit.Assert.*;
-import org.junit.Before;
+import static org.junit.Assert.assertEquals;
 
+import java.sql.Date;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class BTNTest {
@@ -52,9 +54,29 @@ public class BTNTest {
 	public void testCalculaPrincipalAtualizado() {
 		double precoUnitarioDolar = 10.123123;
 		double precoUnitarioAtualizado = 12.123123;
-		int quantidade = 5;
-		
+		int quantidade = 5;		
 		assertEquals(60.615615, btn.calculaPrincipal(precoUnitarioDolar,
 				precoUnitarioAtualizado, quantidade),0.000001);
+	}
+	
+	@Test
+	public void testDescobreDeflatorDia1() {
+		Date dia1 = new Date(12,15,89);	
+		double dolar = 10.123123;
+		assertEquals(7.815050956, btn.descobreDeflator(dia1, dolar));
+	}
+	
+	@Test
+	public void testDescobreDeflatorDia2() {
+		Date dia2 = new Date(9,18,90);
+		double dolar = 10.123123;
+		assertEquals(7.815050956, btn.descobreDeflator(dia2, dolar));
+	}
+	
+	@Test
+	public void testDescobreDeflatorDia3() {
+		Date dia3 = new Date(11,15,90);		
+		double dolar = 10.123123;
+		assertEquals(7.815050956, btn.descobreDeflator(dia3, dolar));
 	}
 }
