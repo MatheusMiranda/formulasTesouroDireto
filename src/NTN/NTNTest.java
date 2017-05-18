b462dd5c316867584dc81fd0fa6b12e01cb81f23
arthurTemporim <arthurrtl@gmail.com>
2017-05-18 20:25:31 -0300

    Create test 'calculaJuros' in NTN.
    Signed-off-by: arthurTemporim <arthurrtl@gmail.com>

--- src/NTN/NTNTest.java
+++ src/NTN/NTNTest.java
@@ -2,11 +2,8 @@ package NTN;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
-import BTN.BTN;
 
 public class NTNTest {
 	
@@ -18,12 +15,18 @@ public class NTNTest {
 	}
 	
 	@Test
-	public void calculaPrecoUnitarioDeAmortizacao() {
+	public void testCalculaPrecoUnitarioDeAmortizacao() {
 		double dolar1 = 10.123123;
 		double dolar2 = 20.321321;
-		double valorLiquido = 1.321321;
-		
+		double valorLiquido = 1.321321;		
 		assertEquals(0.658219, ntn.calculaPrecoUnitarioDeAmortizacao(dolar1, dolar2, valorLiquido),0.000001);
 	}
 	
+	@Test
+	public void testCalculaJuros() {
+		double precoUnitarioAmortizado = 0.658219;
+		double somatoriaDeJuros = 10.123123;
+		
+		assertEquals(6.663231, ntn.calculaJuros(precoUnitarioAmortizado, somatoriaDeJuros).000001);
+	}	
 }
