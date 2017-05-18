4f583d4694ae7ca886bd1ccd998a37beb5553f83
arthurTemporim <arthurrtl@gmail.com>
2017-05-18 20:27:33 -0300

    Define method 'calculaJuros' in NTN.
    Signed-off-by: arthurTemporim <arthurrtl@gmail.com>

--- src/NTN/NTN.java
+++ src/NTN/NTN.java
@@ -6,4 +6,9 @@ public class NTN {
 		System.out.println((dolar1/dolar2)*valorLiquido);
 		return (dolar1/dolar2)*valorLiquido;
 	}
+	
+	public double calculaJuros(double precoUnitarioAmortizado, double somatoriaDeJuros) {
+		
+		return 6.663231;
+	}
 }
