bd426360b7e0d32909eff0f1ab0fc3e03341cf55
arthurTemporim <arthurrtl@gmail.com>
2017-05-18 17:15:30 -0300

    Create NTN class, and NTN test.
    Signed-off-by: arthurTemporim <arthurrtl@gmail.com>

---
+++ src/NTN/NTN.java
@@ -0,0 +1,5 @@
+package NTN;
+
+public class NTN {
+
+}
---
+++ src/NTN/NTNTest.java
@@ -0,0 +1,29 @@
+package NTN;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import BTN.BTN;
+
+public class NTNTest {
+	
+	NTN ntn;
+	
+	@Before
+	public void setUp() throws Exception {
+		ntn = new NTN();
+	}
+	
+	@Test
+	public void calculaPrecoUnitarioDeAmortizacao() {
+		double dolar1 = 10.123123;
+		double dolar2 = 20.321321;
+		double valorLiquido = 1.321321;
+		
+		assertEquals(0.377011, ntn.calculaPrecoUnitarioDeAmortizacao(dolar1, dolar2, valorLiquido),0.000001);
+	}
+	
+}
