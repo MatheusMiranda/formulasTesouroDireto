5a2da32c6f1a7d89e3ec8d242dd9d981f39b4f6f
arthurTemporim <arthurrtl@gmail.com>
2017-05-18 21:11:05 -0300

    Define method 'calculaValorUnitarioJuros' in NTN.
    Signed-off-by: arthurTemporim <arthurrtl@gmail.com>

--- src/NTN/NTN.java
+++ src/NTN/NTN.java
@@ -13,4 +13,8 @@ public class NTN {
 	public double calculaValorNominalAtualizado(double dolar1, double dolar2, double valorDeEmissao) {		
 		return (dolar1/dolar2)*valorDeEmissao;
 	}
+	
+	public double calculaValorUnitarioJuros(double fator, double valorNominalAtualizado) {
+		return 6.663231;
+	}
 }
